7ca2d3b1738eb4e82c580b0e1daa73ae3e5d5c46
bob-g12 <sasakiT1019s@outlook.jp>
2024-01-03 04:27:59 +0900

    viewsに解説コメント追加

--- daily_report/urls.py
+++ daily_report/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),#トップページへのリンク
     path('write/', views.write, name='write'),#投稿ページへのリンク
     path('edit/<int:post_id>/',views.edit, name='edit'),
+    path('review/<int:post_id>/', views.review, name='review'),
     path('exclude/<int:post_id>/', views.exclude, name='exclude'),
 ]
--- daily_report/views.py
+++ daily_report/views.py
@@ -8,60 +8,69 @@ from django.shortcuts import redirect ,get_object_or_404
 
 from .forms import WriteForm 
 
-from django.forms import ModelForm
-  
+# トップページを表示する機能
 class IndexView(View):
     def get(self,request):
-        
+        # 記録してある投稿の全データ
         queryset = Article.objects.all().order_by('-created_at')
-
+        # トップページのhtmlへ記録データを乗せ移行
         return render(request, 'daily_report/post.html', {'posts': queryset})
 
 
 index = IndexView.as_view()
-
+# 投稿機能
 class WriteView(View):
+    # 新規入力画面へ
     def get(self, request):
         return render(request, 'daily_report/write.html', {'form': WriteForm})
-
-    def post(self, request, *args, **kwargs):
+    # 投稿機能
+    def post(self, request):
         # formに書いた内容を格納する
         form = WriteForm(request.POST)
         # 保存する前に一旦取り出す
         post = form.save(commit=False)
         # 保存
         post.save()
-        # indexのviewに移動
+        # トップ画面へ
         return redirect(to='index')
     
 write = WriteView.as_view()
 
+# 編集処理のクラス
 class editView(View):
+    # 編集画面を表示するための処理
     def get(self, request, post_id):
-        print("EEEE")
+        # データベースにある対象のデータのidを一覧から引き受ける(post_id)
+        # modelのArticleからid(post_id)を参照して対象のデータを取得
         post = get_object_or_404(Article, pk=post_id)
+        # 入力フォームに取得したデータを埋め込んで表示
         edit_form = WriteForm(instance=post)
+        # edit.htmlを表示とともに取得したデータと、それを埋め込んだフォームと送る
         return render(request, 'daily_report/edit.html', {'form': edit_form,'post':post})
-
-    def post(self, request, post_id ,*args, **kwargs):
+    # 編集したデータを記録する処理
+    def post(self, request, post_id):
+        # 編集結果を編集元に反映するためにidを取得
         post = get_object_or_404(Article, pk=post_id)
-        # formに書いた内容を格納する
+        # formに書いた内容を編集元へ更新(instance=post)
         form = WriteForm(request.POST, instance=post)
         # 保存する前に一旦取り出す
         post = form.save(commit=False)
         # 保存
         post.save()
-        # indexのviewに移動
+        # トップ画面へ
         return redirect(to='index')
 
 edit = editView.as_view()
 
-def exclude(request, post_id):
-    post = get_object_or_404(Article, pk=post_id)
-    post.delete()
+def review(request, post_id):
+    return render(request, 'daily_report/delete.html',{'post_id':post_id})
 
-    posts =  Article.objects.all()
-    context = {
-        'articles': posts,
-    }
-    return render(request, 'daily_report/post.html', context)
+#削除の機能
+def exclude(request, post_id): 
+    # 処理したいデータを指定
+    post = get_object_or_404(Article, pk=post_id)
+    # キミ消す
+    post.delete()
+    # トップ画面へ
+    return redirect(to='index')
+
