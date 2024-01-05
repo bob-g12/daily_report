5709d4099b353220827c3e320ab499628f08d618
bob-g12 <sasakiT1019s@outlook.jp>
2024-01-06 04:37:13 +0900

    日付登録をカレンダー選択に変更

--- daily_report/forms.py
+++ daily_report/forms.py
@@ -7,4 +7,20 @@ class WriteForm(forms.ModelForm):
         #モデルを指定
         model = Article
         #フォームとして表示したいカラムを指定
-        fields = ('day','name','car_number','weather','start','end','oiling','oiling2','oil','text')
+        fields = (
+            'day',
+            'name',
+            'car_number',
+            'weather',
+            'start',
+            'end',
+            'oiling',
+            'oiling2',
+            'oil',
+            'text'
+            )
+        widgets = {
+            'day': forms.NumberInput(attrs={
+                "type": "date"
+            })
+        }
