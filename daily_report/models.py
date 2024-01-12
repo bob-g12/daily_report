5751501c6e2a93a487fd13d5363a3e05ec25f3b1
bob-g12 <sasakiT1019s@outlook.jp>
2024-01-12 17:21:22 +0900

    max関数の設定/gitignoreの設定

--- daily_report/models.py
+++ daily_report/models.py
@@ -14,17 +14,17 @@ class Article(models.Model):
         db_table = 'posts'
         verbose_name = 'verbose_name'
     #項目作成
-    day = models.DateField('稼働日',blank=True, null=True)
+    day = models.DateField('稼働日',blank=True, null=True,max_length=10)
     name = models.CharField('運転者名', max_length=20, blank=True, null=True)
     car_number = models.CharField('車両No', max_length=5, blank=True, null=True)
     weather = models.CharField('天気', max_length=5, blank=True, null=True)
-    start = models.TimeField('始業時間',blank=True, null=True)	
-    end = models.TimeField('終業時間',blank=True, null=True)
+    start = models.TimeField('始業時間',blank=True, null=True,max_length=10)	
+    end = models.TimeField('終業時間',blank=True, null=True,max_length=10)
     oiling = models.FloatField('給油',max_length=4,blank=True, null=True)
     oiling2 = models.FloatField('給油',max_length=4,blank=True, null=True)
     oil = models.FloatField('オイル',max_length=4,blank=True, null=True)
     text = models.CharField(verbose_name='備考欄', max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField(verbose_name="作成日時",default=timezone.now, blank=True, null=True)
+    created_at = models.DateTimeField(verbose_name="作成日時",default=timezone.now, blank=True, null=True, max_length=255)
     id = models.CharField(primary_key=True, default=id_create, editable=False, max_length=255)
 
     def __str__(self):
