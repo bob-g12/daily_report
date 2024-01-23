22c729c4e44df2bd2cbbd7ee708b4b132ae484fd
bob-g12 <sasakiT1019s@outlook.jp>
2024-01-24 00:30:20 +0900

    account_appの設定修正

--- project_folder/settings.py
+++ project_folder/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'daily_report.apps.DailyReportConfig',
-    'AccountsConfig'
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
